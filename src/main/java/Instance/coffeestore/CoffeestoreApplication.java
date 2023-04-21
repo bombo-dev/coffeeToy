8c8673145f890b6798ed16727669e7d53457f9fc
Bombo <bombo96@chungwoon.ac.kr>
2023-04-21 16:24:30 +0900

    git init

---
+++ src/main/java/Instance/coffeestore/CoffeestoreApplication.java
@@ -0,0 +1,13 @@
+package Instance.coffeestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Instance/coffeestore/CoffeestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package Instance.coffeestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
